c793dc75e283fa4c1ab1c459aa1cfb8eb27971d8
Ethan Groene <ethangroene@gmail.com>
2022-10-31 22:30:31 -0600

    Simplify funcs to make favs modal (dis)appear

--- js/main.js
+++ js/main.js
@@ -116,8 +116,6 @@ favsFunctionality();
 // Run on click of remove from favs btn (event listener)
 
 // FAV MODAL JS
-const modalOpen = '[data-open]';
-const modalClose = '[data-close]';
 const isVisible = 'is-visible';
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
@@ -125,14 +123,19 @@ const closeModal = document.querySelectorAll(modalClose);
 // Modal/Full site modal 'open buttons'
 for (const elem of openModal) {
     elem.addEventListener('click', function() {
-        const modalId = this.dataset.open; // this refers to parent element, which is elem here. It then accesses datasets that are followed by 'open'. Don't use arrow func here, so that this keyword works;
-        document.getElementById(modalId).classList.add(isVisible); // this adds .is-visible as a class to everything in document with data-open="about" or "contact"
+        document.getElementById('favs').classList.add(isVisible);
     })
 }
 
 // Remove isVisible class from elements in HTML with data-close attribute upon click:
+/* for (const elem of closeModal) {
+    elem.addEventListener('click', function() {
+        this.parentElement.parentElement.parentElement.parentElement.classList.remove(isVisible);
+    })
+} */
+
 for (const elem of closeModal) {
     elem.addEventListener('click', function() {
-        this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+        document.getElementById('favs').classList.remove(isVisible);
     })
 }
